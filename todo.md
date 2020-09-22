f55f43d3b2a5d3da7caa3bc90aad415866059db6
Alexander Parreira <alexander.o.parreira@gmail.com>
2020-09-21 21:00:21 -0300

    First commit

---
+++ todo.md
@@ -0,0 +1,25 @@
+1.Tweets
+    ->Creating
+        ->Text
+        ->Image -> Media Storage Server
+    ->Delete
+    ->Retweeting
+
+2. Users
+    ->Register
+    ->Login
+    ->Logout
+    ->Profile
+        ->Image?
+        ->Text?
+    ->Feed
+        ->User's feed only?
+        ->User's who they follow?
+
+3.Following / Followes
+
+
+Long term todos
+- Notification
+- DM
+- Explore -> finfing hashtags
---
+++ tweets/views.py
@@ -0,0 +1,44 @@
+import random
+from django.shortcuts import render
+from django.http import HttpResponse,Http404,JsonResponse
+
+from .models import Tweet
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    #return HttpResponse ("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+     REST API VIEW
+     Consume by javascript or Swift or Java/Android
+     return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list=[{'id': x.id, 'content': x.content, "likes": random.randint(0,123)} for x in qs]
+    data={
+        "isUser":False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+def twet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+     REST API VIEW
+     Consume by javascript or Swift or Java/Android
+     return json data
+    """
+    data = {
+        "id":tweet_id,
+        #"image_path": obj.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+            data['message'] = "Not Found"
+            status = 404
+
+    return JsonResponse(data, status=status) #json.dumps content_type = 'application/json'
