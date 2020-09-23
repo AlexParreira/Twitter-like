f3a4b2def269be402fbc67e90477adc6fc50dcbd
Alexander Parreira <alexander.o.parreira@gmail.com>
2020-09-23 12:50:53 -0300

    form.html part2

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'y0uhcjd$&-vfk3u+ylscwjzngw13x33gwuva!1p^a!2v$=+chc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.cfe.sh']
 
 
 # Application definition
